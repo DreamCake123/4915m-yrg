ea9e064f95bff8306d3e7548406008227fd8516a
Wai-Yin MAN <manyin201612@gmail.com>
2023-06-12 01:03:05 +0800

    BonVer2

--- YRG_BLL/Properties/AssemblyInfo.cs
+++ YRG_BLL/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("143a7368-62af-41a7-aeae-4485b2a49462")]
+[assembly: Guid("9dd5ca2a-c960-4b49-9f26-4fa961f5231c")]
 
 // Version information for an assembly consists of the following four values:
 //
